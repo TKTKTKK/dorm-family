e2cea9ed152477869f20d3919a6653e8764ab5f4
efcndi <zws_3042b>
2017-12-12 16:18:53 +0800

    initial commit

--- portal/src/main/resources/sql/20171204_tb_mtx_product.sql
+++ portal/src/main/resources/sql/20171204_tb_mtx_product.sql
@@ -17,9 +17,6 @@ CREATE TABLE `tb_mtx_product` (
   PRIMARY KEY (`uuid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 产品管理
-INSERT INTO `tb_platform_permit` VALUES ('2017120410000002', 'MENU', '产品管理', 'admin/wefamily/MtxProduct', '2015050510000011', '1102', 'APP', '2015-05-20 19:31:10', 'sys', '2015-05-20 19:31:10', 'sys', '1', 'N', '');
-
 -- 创建预订表
 CREATE TABLE `tb_mtx_reserve` (
   `uuid` varchar(32) NOT NULL,
@@ -40,9 +37,6 @@ CREATE TABLE `tb_mtx_reserve` (
   PRIMARY KEY (`uuid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 咨询管理
-INSERT INTO `tb_platform_permit` VALUES ('2017120510000005', 'MENU', '咨询管理', 'admin/wefamily/mtxReserveManage', '2015050510000011', '1104', 'APP', '2015-05-20 19:31:10', 'sys', '2015-05-20 19:31:10', 'sys', '1', 'N', '');
-
 -- 创建留言表
 CREATE TABLE `tb_mtx_consult` (
   `uuid` varchar(32) NOT NULL,
@@ -57,9 +51,6 @@ CREATE TABLE `tb_mtx_consult` (
   `delind` char(1) NOT NULL,
   PRIMARY KEY (`uuid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 留言管理
-INSERT INTO `tb_platform_permit` VALUES ('2017120610000002', 'MENU', '留言管理', 'admin/wefamily/mtxConsultManage', '2015050510000011', '1105', 'APP', '2015-05-20 19:31:10', 'sys', '2015-05-20 19:31:10', 'sys', '1', 'N', '');
 
 -- 创建留言详情表
 CREATE TABLE `tb_mtx_consult_detail` (
@@ -84,10 +75,6 @@ INSERT INTO tb_common_code VALUES ('2017120700000005', null, null, 'IDENTITY_COD
 INSERT INTO tb_common_code VALUES ('2017120700000007', null, null, 'ANWSER_OR_NOT', 'ANWSER_RECENT', '已回复', null,'', '7', '2014-11-30 21:44:31', 'sys', '2014-11-30 21:44:31', 'sys', '1', '0');
 INSERT INTO tb_common_code VALUES ('2017120700000008', null, null, 'ANWSER_OR_NOT', 'NO_ANWSER', '未回复', null,'', '8', '2014-11-30 21:44:31', 'sys', '2014-11-30 21:44:31', 'sys', '1', '0');
 
-
--- 会员管理
-INSERT INTO `tb_platform_permit` VALUES ('2017120810000001', 'MENU', '会员管理', 'admin/wefamily/mtxMemberManage', '2015050510000011', '1106', 'APP', '2015-05-20 19:31:10', 'sys', '2015-05-20 19:31:10', 'sys', '1', 'N', '');
-
 -- 商品兑换表
 CREATE TABLE `tb_mtx_good_exchange` (
   `uuid` varchar(32) NOT NULL,
@@ -105,11 +92,6 @@ CREATE TABLE `tb_mtx_good_exchange` (
   PRIMARY KEY (`uuid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 商品兑换管理
-INSERT INTO `tb_platform_permit` VALUES ('2017120910000002', 'MENU', '商品兑换管理', 'admin/wefamily/mtxGoodManage', '2015050510000011', '1108', 'APP', '2015-05-20 19:31:10', 'sys', '2015-05-20 19:31:10', 'sys', '1', 'N', '');
-
-
-
 -- 积分消费记录表
 CREATE TABLE `tb_mtx_point_record` (
   `uuid` varchar(32) NOT NULL,
@@ -125,10 +107,6 @@ CREATE TABLE `tb_mtx_point_record` (
   `delind` char(1) NOT NULL,
   PRIMARY KEY (`uuid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 积分管理
-INSERT INTO `tb_platform_permit` VALUES ('2017120910000001', 'MENU', '积分管理', 'admin/wefamily/mtxPointManage', '2015050510000011', '1107', 'APP', '2015-05-20 19:31:10', 'sys', '2015-05-20 19:31:10', 'sys', '1', 'N', '');
-
 
 alter table tb_wp_user add column `province`  varchar(60) default null AFTER contactno;
 alter table tb_wp_user add column `city`  varchar(60) default null AFTER province;
