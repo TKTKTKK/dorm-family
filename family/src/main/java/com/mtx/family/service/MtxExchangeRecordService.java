e8296091b435c8bc8352d7925d128445a44376db
efcndi <zws_3042b>
2017-12-18 09:12:43 +0800

    initial commit

--- family/src/main/java/com/mtx/family/service/MtxExchangeRecordService.java
+++ family/src/main/java/com/mtx/family/service/MtxExchangeRecordService.java
@@ -4,14 +4,10 @@ import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
 import com.github.miemiedev.mybatis.paginator.domain.PageList;
 import com.mtx.common.base.BaseService;
 import com.mtx.family.entity.MtxExchangeRecord;
-import com.mtx.family.entity.MtxPartsCenter;
 import com.mtx.family.mapper.MtxExchangeRecordMapper;
-import com.mtx.family.mapper.MtxPartsCenterMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 
 @Service
