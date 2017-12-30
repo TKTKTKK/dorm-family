1b07c286ff2e247baff67dbacc04ccaba64e7943
efcndi <zws_3042b>
2017-12-30 13:42:42 +0800

    initial commit

--- portal/src/main/resources/sql/20171229_tb_mtx_activity.sql
+++ portal/src/main/resources/sql/20171229_tb_mtx_activity.sql
@@ -1,4 +1,4 @@
 -- 中奖总人数
 alter table tb_mtx_activity add  column totalLuckyCount int(3) DEFAULT  NULL AFTER status;
 -- 每轮中奖人数
-alter table tb_mtx_activity add  column everyLuckyCount int(3) DEFAULT  NULL AFTER totalParticipant;
+alter table tb_mtx_activity add  column everyLuckyCount int(3) DEFAULT  NULL AFTER totalLuckyCount;
