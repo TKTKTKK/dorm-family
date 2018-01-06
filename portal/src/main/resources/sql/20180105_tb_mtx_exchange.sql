2f03a203da526ea6f537152525dee57c0c228e9f
admin <shiyu@globalph.cn>
2018-01-06 16:10:16 +0800

    细节修改

---
+++ portal/src/main/resources/sql/20180105_tb_mtx_exchange.sql
@@ -0,0 +1,6 @@
+
+-- 修改详情的字符集
+ALTER TABLE tb_mtx_reserve  MODIFY detail VARCHAR(256) CHARACTER SET utf8mb4;
+
+ALTER TABLE tb_mtx_exchange_record  MODIFY detail VARCHAR(256) CHARACTER SET utf8mb4;
+
