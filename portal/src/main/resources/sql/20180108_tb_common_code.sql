b96bbef579e589a31910c7bbf667fef677afe0a1
admin <shiyu@globalph.cn>
2018-01-08 09:46:42 +0800

    细节修改

--- portal/src/main/resources/sql/20180108_tb_common_code.sql
+++ portal/src/main/resources/sql/20180108_tb_common_code.sql
@@ -1,6 +1,6 @@
 
--- 把commoncode里的已转代理商改为转代理商
-update tb_common_code  set codevalue='转代理商' where uuid ='2017121800000003';
+-- 把commoncode里的已转代理商改为转代理商处理
+update tb_common_code  set codevalue='转代理商处理' where uuid ='2017121800000003';
 
 
 
