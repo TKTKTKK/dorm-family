a50cf2c4130ac6633797c9ce94678d99180cee4f
TK <295904906@qq.com>
2018-04-10 00:01:44 +0800

    First Class

---
+++ family/src/main/java/com/mtx/family/entity/testGitHub.java
@@ -0,0 +1,16 @@
+package com.mtx.family.entity;
+
+/**
+ * Created by Administrator on 2018/4/9.
+ */
+public class testGitHub {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
